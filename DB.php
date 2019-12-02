d5897fef88fefb3cb7024021c38e3dcca90c2582
KANAZAWA1 <49836286+KANAZAWA1@users.noreply.github.com>
2019-12-02 20:55:23 +0800

    封装对数据库的增删改
    添加数据已完成

--- DB.php
+++ DB.php
@@ -21,7 +21,22 @@
         private function __clone(){}
         //返回数据库实例对象
         public static function getDb($table_name){
-
+            self::$table_name = $table_name;
+            //判断如果静态属性中是否有类对象  如果没有则，实例化该类 赋予 静态属性
+            //如此开辟了一块静态空间用来存储实例化对象 类外只需调用一次即可 节省了资源
+            if(!(self::$instance instanceof self)){
+                self::$instance = new self;
+            }
+            return self::$instance;
+        }
+        //添加数据
+        function fb($table_name, $data){
+            $keys = implode(',', array_keys($data));
+            $value = "'".implode("','", array_values($data))."'";
+            $sql = "insert into $table_name ($keys) values($value)";
+            $r = $this->pdo->exec($sql);
+            $this->getErrorInfo();
+            return $r;
         }
     }
 ?>
